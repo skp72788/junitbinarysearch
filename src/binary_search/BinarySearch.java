500403de42682eba2bdbac56052703452bafc772
Sam Parupsky <samparupsky@gmail.com>
2026-09-22 10:47:44 -0500

    modify BinarySearch.java close #2

--- src/binary_search/BinarySearch.java
+++ src/binary_search/BinarySearch.java
@@ -4,12 +4,19 @@ public class BinarySearch {
 	
 	static int binarySearch(int[] array, int target) {
 		
+		
 		int min = 0;
 		int max = array.length - 1;
+			
 		int mid = (min + max) / 2;
 		
 		
 		while(true) {
+			
+			mid = (min + max) / 2;
+			if(min > max) {
+				return -1;
+			}
 			if(array[mid] == target) {
 				return mid;
 			}
@@ -19,10 +26,8 @@ public class BinarySearch {
 			else if(array[mid] > target) {
 				max = mid - 1;
 			}
-			mid = (min + max) / 2;
-			if(min > max) {
-				return -1;
+			
 			}
 		}
 	}
-}
+
